08de3b26d485c9f0cb0a033fa79d54efae66f7d8
JenniferCubillasCayahuallpa <jennifer.cubillas.c@vallegrande.edu.pe>
2024-05-30 16:37:18 -0500

    node10

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const path = require('path')
 const PORT = 3000;
 const ip = 'localhost'
 
-app.use(express,static('AS232S2_T07/'))
+app.use(express.static('public'))
 
 app.get('/',(req,res) =>{
     res,sendFile(path,join(__dirname, 'index.html'))
 } )
 
-app.listen(PORT,() => {
-    console.log('Server en http://${ip}:${PORT}')
+app.listen(PORT, () => {
+    console.log(`Server en http://${ip}:${PORT}`)
 })
